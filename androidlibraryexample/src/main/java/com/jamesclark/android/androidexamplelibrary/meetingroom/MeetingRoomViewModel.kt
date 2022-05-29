82de499c1eb46b52276676283e8d0bb80546ea17
James Clark <james@isaziconsulting.co.za>
2022-05-29 20:31:49 +0200

    Add API methods

--- androidlibraryexample/src/main/java/com/jamesclark/android/androidexamplelibrary/meetingroom/MeetingRoomViewModel.kt
+++ androidlibraryexample/src/main/java/com/jamesclark/android/androidexamplelibrary/meetingroom/MeetingRoomViewModel.kt
@@ -4,8 +4,10 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import com.google.gson.Gson
+import com.jamesclark.android.androidexamplelibrary.meetingroom.data.Availability
 import com.jamesclark.android.androidexamplelibrary.meetingroom.data.Floor
 import com.jamesclark.android.androidexamplelibrary.meetingroom.data.Floors
+import com.jamesclark.android.androidexamplelibrary.meetingroom.data.Room
 import kotlinx.coroutines.*
 import java.io.File
 
@@ -16,11 +18,122 @@ class MeetingRoomViewModel(
     val cachePath = getApplication<Application>().cacheDir.canonicalPath + "/" + "floors.json"
     val errorMessage = MutableLiveData<String>()
     val floorList = MutableLiveData<List<Floor>>()
+    val roomList = MutableLiveData<List<Room>>()
+    val availableTimesList = MutableLiveData<List<Availability>>()
     val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
         onError("Exception handled: ${throwable.localizedMessage}")
     }
     val loading = MutableLiveData<Boolean>()
     var job: Job? = null
+
+    fun getAvailableTimes(rooms: List<Room>) {
+        val roomIDSet = rooms.map { r -> r.id }.toSet()
+        job = CoroutineScope(Dispatchers.IO + exceptionHandler).launch {
+            if (MeetingRoomAPI.isInternetConnected(getApplication())) {
+                // load from API if internet is connected
+                val response = repository.getAllFloors()
+                withContext(Dispatchers.Main) {
+                    if (response.isSuccessful) {
+                        response.body()?.let { body ->
+                            FileUtils.saveToCacheFile(Gson().toJson(body), cachePath)
+                            availableTimesList.postValue(body.floors.flatMap { f -> f.rooms }
+                                .filter { r -> roomIDSet.contains(r.id) }
+                                .flatMap { r -> r.availability })
+                            loading.value = false
+                        }
+                    } else {
+                        onError("Error : ${response.message()} ")
+                    }
+                }
+            } else {
+                // otherwise load from local cache if app is offline
+                if (File(cachePath).exists()) {
+                    val floors =
+                        Gson().fromJson(FileUtils.readJsonFile(cachePath), Floors::class.java)
+                    withContext(Dispatchers.Main) {
+                        availableTimesList.postValue(floors.floors.flatMap { f -> f.rooms }
+                            .filter { r -> roomIDSet.contains(r.id) }
+                            .flatMap { r -> r.availability })
+                        loading.value = false
+                    }
+                } else {
+                    // no cached data to load
+                    roomList.postValue(emptyList())
+                    loading.value = false
+                }
+            }
+        }
+    }
+
+    fun getRooms(floor: Floor) {
+        job = CoroutineScope(Dispatchers.IO + exceptionHandler).launch {
+            if (MeetingRoomAPI.isInternetConnected(getApplication())) {
+                // load from API if internet is connected
+                val response = repository.getAllFloors()
+                withContext(Dispatchers.Main) {
+                    if (response.isSuccessful) {
+                        response.body()?.let { body ->
+                            FileUtils.saveToCacheFile(Gson().toJson(body), cachePath)
+                            roomList.postValue(body.floors.filter { f -> f.id == floor.id }
+                                .flatMap { f -> f.rooms })
+                            loading.value = false
+                        }
+                    } else {
+                        onError("Error : ${response.message()} ")
+                    }
+                }
+            } else {
+                // otherwise load from local cache if app is offline
+                if (File(cachePath).exists()) {
+                    val floors =
+                        Gson().fromJson(FileUtils.readJsonFile(cachePath), Floors::class.java)
+                    withContext(Dispatchers.Main) {
+                        roomList.postValue(floors.floors.filter { f -> f.id == floor.id }
+                            .flatMap { f -> f.rooms })
+                        loading.value = false
+                    }
+                } else {
+                    // no cached data to load
+                    roomList.postValue(emptyList())
+                    loading.value = false
+                }
+            }
+        }
+    }
+
+    fun getAllRooms() {
+        job = CoroutineScope(Dispatchers.IO + exceptionHandler).launch {
+            if (MeetingRoomAPI.isInternetConnected(getApplication())) {
+                // load from API if internet is connected
+                val response = repository.getAllFloors()
+                withContext(Dispatchers.Main) {
+                    if (response.isSuccessful) {
+                        response.body()?.let { body ->
+                            FileUtils.saveToCacheFile(Gson().toJson(body), cachePath)
+                            roomList.postValue(body.floors.flatMap { f -> f.rooms })
+                            loading.value = false
+                        }
+                    } else {
+                        onError("Error : ${response.message()} ")
+                    }
+                }
+            } else {
+                // otherwise load from local cache if app is offline
+                if (File(cachePath).exists()) {
+                    val floors =
+                        Gson().fromJson(FileUtils.readJsonFile(cachePath), Floors::class.java)
+                    withContext(Dispatchers.Main) {
+                        roomList.postValue(floors.floors.flatMap { f -> f.rooms })
+                        loading.value = false
+                    }
+                } else {
+                    // no cached data to load
+                    roomList.postValue(emptyList())
+                    loading.value = false
+                }
+            }
+        }
+    }
 
     fun getAllFloors() {
         job = CoroutineScope(Dispatchers.IO + exceptionHandler).launch {
